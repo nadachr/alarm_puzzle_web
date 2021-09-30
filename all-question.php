b25873ecb215093255d762ff991560080e8c2e06
nadachr <nnada.chr@gmail.com>
2021-09-30 22:40:29 +0700

    Finish

--- all-question.php
+++ all-question.php
@@ -40,7 +40,7 @@ $i = 1 + $start;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css">
-    <title>Alarm-Puzzle | Login</title>
+    <title>Alarm-Puzzle | All Question</title>
 </head>
 
 <body>
--- edit-question.php
+++ edit-question.php
@@ -62,7 +62,7 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css">
-    <title>Alarm-Puzzle | Login</title>
+    <title>Alarm-Puzzle | Edit</title>
 </head>
 
 <body>
